4da830417ef9f4fb0223c247fbe38bd2450de814
Jordan1122 <rozgonyibalint@gmail.com>
2023-03-29 00:21:12 +0300

    Install and setup Navigation2 stack

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ echo 'source "/opt/ros/$ROS_DISTRO/setup.bash"' >> ~/.bashrc
 cd /test_ws && colcon build
 source /test_ws/install/local_setup.bash
 
+source /usr/share/gazebo/setup.bash
+export TURTLEBOT3_MODEL=waffle
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$ROS_DISTRO/share/turtlebot3_gazebo/models
+
 exec "$@"
