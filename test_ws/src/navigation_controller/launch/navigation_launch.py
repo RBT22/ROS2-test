a67fe655a1f9ff62f763a7a30d0ee3d60410a703
Jordan1122 <rozgonyibalint@gmail.com>
2023-03-29 01:29:45 +0300

    Add navigator node to launch file

--- test_ws/src/navigation_controller/launch/navigation_launch.py
+++ test_ws/src/navigation_controller/launch/navigation_launch.py
@@ -32,10 +32,17 @@ def generate_launch_description():
         }.items(),
     )
 
+    navigator = Node(
+        package="navigation_controller",
+        executable="navigator",
+        output="screen",
+    )
+
     ld = LaunchDescription()
 
     ld.add_action(use_rviz_arg)
 
     ld.add_action(nav2_bringup)
+    ld.add_action(navigator)
 
     return ld
